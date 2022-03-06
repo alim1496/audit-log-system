8c63a421aed2fa0e0cbb9d30597afac692059681
alim1496 <alim1496@gmail.com>
2022-03-07 03:57:37 +0600

    design login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { Avatar, Button, Grid, Paper, TextField } from "@mui/material";
+import LockOpenIcon from '@mui/icons-material/LockOpen';
 
-const Login = () => (
-    <div>
-        Hello from login
-    </div>
-);
+const Login = () => {
+    const paperStyle = {
+        padding: 20,
+        width: 400,
+        margin: "50px auto"
+    };
+
+    const spaced = {
+        margin: "10px 0"
+    };
+
+    const spaceBottom = {
+        marginBottom: "5px"
+    };
+
+    return (
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid>
+                    <Avatar><LockOpenIcon/></Avatar>
+                    <h3 style={spaced}>Account Login</h3>
+                </Grid>
+                <TextField style={spaceBottom} label="Username" placeholder="Enter username" required fullWidth />
+                <TextField style={spaceBottom} label="Password" placeholder="Enter password" type="password" fullWidth required />
+                <Button style={spaced} type="submit" color="primary" variant="contained" fullWidth>Login</Button>
+            </Paper>
+        </Grid>
+    );
+};
 
 export default Login;
